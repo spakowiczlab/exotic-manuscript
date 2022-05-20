188ee4a26fa208824434c23dc3b0565a207206a7
hoyd44 <rhoyd44@gmail.com>
2022-05-20 13:16:46 -0400

    Figure 1 tweaks, first round

--- manuscripts/exoTiC/scripts/network_find-shortest-paths.R
+++ manuscripts/exoTiC/scripts/network_find-shortest-paths.R
@@ -1,19 +1,21 @@
 library(tidyverse)
 library(igraph)
 
-all.nodes.combos <- readRDS("/fs/ess/PAS1695/projects/exotic/data/network_all-node-combos.RDS")
+# all.nodes.combos <- readRDS("/fs/ess/PAS1695/projects/exotic/data/network_all-node-combos.RDS")
 weighted.edges <- readRDS("/fs/ess/PAS1695/projects/exotic/data/network_igraph-data.RDS")
 
-grabShortPaths <- function(n){
-  tmppath <- shortest_paths(weighted.edges, 
-                            from = all.nodes.combos[1,n],
-                            to = all.nodes.combos[2,n]) 
-  tmp.return <- gsub("vpath.", "", names(unlist(tmppath)))
-  return(tmp.return)
-}
+# grabShortPaths <- function(n){
+#   tmppath <- shortest_paths(weighted.edges, 
+#                             from = all.nodes.combos[1,n],
+#                             to = all.nodes.combos[2,n]) 
+#   tmp.return <- gsub("vpath.", "", names(unlist(tmppath)))
+#   return(tmp.return)
+# }
 
-all.paths <- lapply(1:ncol(all.nodes.combos), grabShortPaths)
+# all.paths <- lapply(1:ncol(all.nodes.combos), grabShortPaths)
 
-saveRDS(all.paths,
-        "/fs/ess/PAS1695/projects/exotic/data/network_all-shortest-paths.RDS")
+centrality <- betweenness(weighted.edges)
+
+saveRDS(centrality,
+        "/fs/ess/PAS1695/projects/exotic/data/network_betweenness-centrality.RDS")
 
